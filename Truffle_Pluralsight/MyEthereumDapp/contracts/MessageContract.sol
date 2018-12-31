4a12b5c64b88eae785757597f8878231f88b87a6
RudrikaFichadiya <44025030+RudrikaFichadiya@users.noreply.github.com>
2018-12-31 18:55:51 +0530

    Add files via upload

---
+++ Truffle_Pluralsight/MyEthereumDapp/contracts/MessageContract.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.2;
+
+contract Sample1{
+    
+        string public message;
+        
+        function setMessage(string setmessage) public{
+            message = setmessage;
+        }
+    
+}
